feca610b8f4919fa0f6aee3aed06ce550b7d1e93
Laramie <laramie.miller@gmail.com>
2026-03-14 17:53:33 -1000

    Chapter 1 completed

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import "net/http"
 
 func main() {
 	httpMux := http.NewServeMux()
+	httpMux.Handle("/app/",
+		http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	httpMux.HandleFunc("/healthz", readinessCheck)
 	http.ListenAndServe(":8080", httpMux)
 
+}
+
+func readinessCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Add("Content-Type:", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
 }
